e527336e73c612931fbb350c3e6462a64dc80947
Kirill Karpov <rin.veber@gmail.com>
2024-03-15 21:01:01 +0400

    add visible password input

--- src/componets/FormLogin/InputFields/InputFields.tsx
+++ src/componets/FormLogin/InputFields/InputFields.tsx
@@ -1,14 +1,21 @@
-import { Box, FormHelperText, Stack } from "@mui/material";
+import React from "react";
+import { Box, FormHelperText, InputAdornment, Stack } from "@mui/material";
 import { Controller, useFormContext } from "react-hook-form";
 import { InputCustom } from "../../../shared/UI/Input/InputCustom";
 import { ButtonLogin } from "../../../shared/UI/Button/ButtonCustom";
 import { IFormLogin } from "../FormLogin";
+import { VisibilityOutlined, VisibilityOffOutlined } from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
 
 export default function InputFields() {
   const {
     control,
     formState: { errors, isValid },
   } = useFormContext<IFormLogin>();
+
+  const [showPassword, setShowPassword] = React.useState(true);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   return (
     <Stack
@@ -56,9 +63,24 @@ export default function InputFields() {
             <>
               <InputCustom
                 placeholder="Пароль"
+                type={showPassword ? "password" : "text"}
                 fullWidth
                 {...fieldsProps}
                 error={!!errors.password}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                    >
+                      {!showPassword ? (
+                        <VisibilityOutlined sx={{ color: "white" }} />
+                      ) : (
+                        <VisibilityOffOutlined sx={{ color: "white" }} />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
               />
               <FormHelperText error={!!errors.password}>
                 {errors.password?.message}
